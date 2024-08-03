b521648dfac914901687be390e9b6e5949775cc6
AgusRakhmat <107832120+AgusRakhmatHaryanto@users.noreply.github.com>
2024-08-03 11:27:31 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 const { PORT } = require('./config/env');
 
-app.listen(PORT, () => {
+app.listen(PORT || 8080, () => {
   console.log(`Server is running on port ${PORT}`);
 });
